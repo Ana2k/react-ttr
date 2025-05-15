e753b6800ff0f10eaac12930c1051ee3b11dc514
Ana2k <anushkachakra@gmail.com>
2025-05-15 17:27:07 +0530

    first commit

---
+++ src/dummy-components/MyApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MyButton from './MyButton'
+
+const MyApp = () => {
+  return (
+    <div>
+      Welcome to my MyApp
+      <MyButton/>
+    </div>
+  )
+}
+
+export default MyApp
---
+++ src/dummy-components/MyButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//react componenets name with capital letter while, 
+//html divs with smaller letters. 
+const MyButton = () => {
+  return (
+    <div>
+      I am a button. 
+    </div>
+  )
+}
+
+export default MyButton
