4968b10026522a0f3afcbf2bd6ede04f15f532c2
Ana2k <anushkachakra@gmail.com>
2025-05-18 18:18:11 +0530

    Auto-update: 2025-05-18 18:18:10

--- src/dummy-components/MyButton.jsx
+++ src/dummy-components/MyButton.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
 //react componenets name with capital letter while, 
+//extra lines that may be needed. 
+//function name with capital letter while, 
+//extra lines that may be needed. 
+//function name with capital letter while, 
+//extra lines that may be needed. 
+//function name with capital letter while, 
+//extra lines that may be needed. 
 
 function MyButton({ count, onClick}){
   return (
