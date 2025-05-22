d7788b0bb0bbf027e66a42e65841a28dbb56280b
Ana2k <anushkachakra@gmail.com>
2025-05-22 09:26:35 +0530

    Prepare App.jsx for tic-tac-toe implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import {useState} from 'react';
-import MyButton from './dummy-components/MyButton';
-export default function MyApp() {
-  //this causes both buttons to update together
-  const [count, setCount] = useState(0);
-  function handleClick(){
-    setCount(count+1);
-  }
+//Following this we will work on tic-tac-toe
+export default function App() {
+
   return (
     <div>
-      <h1> Counters that update together</h1>
-      <MyButton count = {count} onClick = {handleClick}/>
-      <MyButton count = {count} onClick = {handleClick}/>
-      //this is us passing the state down to the button
-      //we are passing the count and the handleClick function
-      //Lifting state up!!
+      <h1>My Clean React App</h1>
     </div>
   );
 }
