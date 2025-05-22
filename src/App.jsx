f203ef90ea38d71ba2dc95205c726a4226da4a2e
Ana2k <anushkachakra@gmail.com>
2025-05-22 09:44:32 +0530

    Auto-update: 2025-05-22 09:44:31

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-export default function App() {
+import { useState } from 'react';
+import Square from './tic-tac-toe components/Square';
 
+export default function App() {
+//SETUP.
   return (
     <div>
       <h1>My Clean React App</h1>
