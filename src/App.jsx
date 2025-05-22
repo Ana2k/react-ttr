cf9b12c88d8353acbd014e71288b975741ca6c35
Ana2k <anushkachakra@gmail.com>
2025-05-22 09:23:04 +0530

    Auto-update: 2025-05-22 09:23:02

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import MyButton from './dummy-components/MyButton';
 export default function MyApp() {
+  //this causes both buttons to update together
   const [count, setCount] = useState(0);
   function handleClick(){
     setCount(count+1);
@@ -10,6 +11,8 @@ export default function MyApp() {
       <h1> Counters that update together</h1>
       <MyButton count = {count} onClick = {handleClick}/>
       <MyButton count = {count} onClick = {handleClick}/>
+      //this is us passing the state down to the button
+      //we are passing the count and the handleClick function
     </div>
   );
 }
