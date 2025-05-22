019b10961d1f8bec268732349ab45260790b6234
Ana2k <anushkachakra@gmail.com>
2025-05-23 00:12:11 +0530

    Auto-update: 2025-05-23 00:12:10 (Commit #2 today)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import { useState } from 'react';
-
-function Square({ value, onSquareClick }){
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
+export default function Square(){
+  return <button className=></button>
 }
