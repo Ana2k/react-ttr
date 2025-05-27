d22b1792e92adece136659cbd8add8c4172e6005
Ana2k <anushkachakra@gmail.com>
2025-05-27 14:24:10 +0530

    Taking Turns #18

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,10 @@ export default function Board(){
   const [squares,setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i){
+    //Debug - i can overwrite the squares without this
+    if(squares[i]){
+      return;
+    }
     const nextSquares = squares.slice();
     if(xIsNext){
       nextSquares[i] = "X";
