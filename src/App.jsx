0345348a017960a593f2a71e548e31c57f0693fb
Ana2k <anushkachakra@gmail.com>
2025-05-27 12:09:21 +0530

    Auto-update: 2025-05-27 12:09:20 (Commit #4 today)

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@
 import { useState } from 'react';
 
 function Square({value}){
-  const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(){
-    const nextSquares = squares.slice();
-    nextSquares[0] = "X";
-    setSquares(nextSquares);
+    console.log("handleclicked");
   }
 
   return (
-    <button className="square">{value}</button>
+    <button className="square" onClick="{handleClick}">{value}</button>
   );
 }
 
