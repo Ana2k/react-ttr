e0c217d987cca916ffab3a87f188dce23f4a1900
Ana2k <anushkachakra@gmail.com>
2025-05-27 16:15:19 +0530

    Auto-update: 2025-05-27 16:15:18 (Commit #19 today)

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,29 @@ export default function Board(){
   const [xIsNext,setXIsNext] = useState(true);
   const [squares,setSquares] = useState(Array(9).fill(null));
 
+  function calculateWinner(squares){
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for(i=0;i<lines.length;i++){
+      const [a,b,c] = lines[i];
+      if(squares[a] && squares[a]===squares[b] && squares[b]===squares[c] && squares[a]===squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   function handleClick(i){
     //Debug - i can overwrite the squares without this
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
       return;
     }
     const nextSquares = squares.slice();
